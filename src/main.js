dcb085e99ad91aa436938018d3bcaa30a79bc522
LIMBO\82637 <826372606@qq.com>
2018-09-11 16:07:06 +0800

    创建项目

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+
+import router from './router'
+import App from './App.vue'
+
+
+import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
+
+new Vue({
+    store,
+    router,
+    render: h => h(App)
+}).$mount('#app');
