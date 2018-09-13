9904fb7f271b574c5b7707651cade76956fd5330
LIMBO\82637 <826372606@qq.com>
2018-09-13 14:43:10 +0800

    添加用户管理store router 建立用户管理目录和文件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 
 import router from './router'
 import App from './App.vue'
+import userAccount from './components/userAccount/userAccount.vue';
 
 
 import store from './store'
@@ -9,6 +10,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(ElementUI);
+Vue.component('userAccount', userAccount);
 
 
 new Vue({
--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import app from './components/app/store';
+import userAccount from './components/userAccount/store';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
-        app
+        app,
+        userAccount
     }
 });
