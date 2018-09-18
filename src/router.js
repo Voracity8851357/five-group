77ddb295751b021219680eec34865126f086102c
LIMBO\82637 <826372606@qq.com>
2018-09-18 09:53:07 +0800

    修复路由

--- src/router.js
+++ src/router.js
@@ -1,20 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import IndexPlatform from "./routes/index-platform";
-import IndexStore from "./routes/index-store";
+import Index from "./routes/index";
 import store from './store.js';
+
+
 import login from './components/app/login.vue';
 import reg from './components/app/reg.vue';
-import petOwners from './components/petowners/pet';
-
+import PetOwners from './components/petowners/pet';
 import ServiceManage from './components/serviceManage/serviceManage.vue'
-
 import ApplyForShop from './components/applyForShop/applyForShop.vue';
 import ShopName from './components/shopName/shopName.vue'
 import UserAccount from './components/userAccount/userAccount';
 import GoodsContainer from "./components/goodsManagement/goodsContainer";
-import orderManagement from './components/orderManagement/orderManagement'
+import orderManagement from './components/orderManagement/orderManagement';
+
 Vue.use(Router);
 export default new Router({
     routes: [
@@ -34,27 +34,8 @@ export default new Router({
             component: reg
         },
         {
-            path: "/indexPlatform",
-            component: IndexPlatform,
-            children: [
-                {
-                    path: '/index/userAccount',
-                    component: UserAccount,
-                },
-                {
-                    path: '/index/shopName',
-                    // component: ShopName,
-                },
-                {
-                    path: '/index/petOwners',
-                    component: petOwners,
-                },
-            ]
-        },
-        {
-            path: "/indexStore",
             path: "/index",
-            component: IndexStore,
+            component: Index,
             children: [
                 {
                     path: '/index/applyForShop',
@@ -70,10 +51,6 @@ export default new Router({
                     component: ServiceManage,
                 },
                 {
-                    path: '/index/petOwners',
-                    // component: petOwners,
-                },
-                {
                     path: '/index/orderManagement',
                     component: orderManagement,
                 },
@@ -87,7 +64,7 @@ export default new Router({
                 },
                 {
                     path: '/index/petOwners',
-                    // component: PetOwners,
+                    component: PetOwners,
                 },
             ]
         }
