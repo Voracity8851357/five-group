e784f29e3589cef6f6cead9643874ea3c592413e
LIMBO\82637 <826372606@qq.com>
2018-09-18 09:46:55 +0800

    no message

--- src/components/userAccount/userAccount.vue
+++ src/components/userAccount/userAccount.vue
@@ -244,7 +244,6 @@
                 this.async_getUsers();
             },
             async handleDeny(index, row) {
-               
                 this.async_putAudit({
                     userStatus: '2',
                     _id: row._id
